a2070dd08ee4ea1b913f3951edec571f7d09fa00
Wasif Zaman <teatocode@gmail.com>
2017-10-29 20:22:42 -0400

    better clustering

--- server/db/Cluster.js
+++ server/db/Cluster.js
@@ -9,7 +9,13 @@ const Cluster = conn.define('cluster', {
 
 function findAndAddToCluster (thought, cluster) {
   return conn.models.thought.findById(thought.id)
-    .then(thought => cluster.addThoughts(thought))
+    .then(thought => {
+      if (thought.clusterId) {
+        return conn.models.thought.findAll({ where: { clusterId: thought.clusterId } })
+          .then(thoughts => Promise.all(thoughts.map(t => cluster.addThoughts(t))))
+      }
+      return cluster.addThoughts(thought)
+    })
 }
 
 Cluster.clusterThoughts = function(thoughts) {
