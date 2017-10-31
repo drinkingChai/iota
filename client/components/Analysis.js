c818dc675292cf2a61a632fee566e6e6d05d15e8
Wasif Zaman <teatocode@gmail.com>
2017-10-31 12:56:22 -0400

    safe

--- client/components/Analysis.js
+++ client/components/Analysis.js
@@ -3,86 +3,71 @@ import { connect } from 'react-redux'
 import { catFrequency, catFrequencyOverTime } from '../helpers'
 import { responsivefy } from '../svghelpers'
 import * as d3 from 'd3'
+import Pack from './charts/Pack'
 
 /* flatten data on frontend or backend?
  * if on front end, I can manipulate and see relations?
 */
 
 class Analysis extends Component {
+  constructor() {
+    super()
+    this.state = {
+      lineSelect: '',
+      packSelect: 'recent'
+    }
+    this.selectPackView = this.selectPackView.bind(this)
+    this.selectLineView = this.selectLineView.bind(this)
+  }
+
   componentDidMount() {
     window.scrollTo(0, 0);
+    if (this.props.thoughts.length) Pack(catFrequencyOverTime(this.props.thoughts).slice(0, 5), '.pie-chart')
   }
 
   componentWillReceiveProps(nextProps) {
-    const data = catFrequencyOverTime(nextProps.thoughts).slice(0, 5)
-    const max = d3.max(data, d => d.count)
-    const color = d3.scaleLinear()
-      .domain([0, 1])
-      .range(['#80deea', '#f44336'])
+    Pack(catFrequencyOverTime(nextProps.thoughts).slice(0, 5), '.pie-chart')
+  }
 
-    const width = 300,
-      height = 300
+  selectPackView(ev) {
+    const { name, value } = ev.target
+    this.setState({ [name]: value })
+    if (value == 'recent') return Pack(catFrequencyOverTime(this.props.thoughts).slice(0, 5), '.pie-chart')
+    return Pack(catFrequencyOverTime(this.props.thoughts), '.pie-chart')
+  }
 
-    const pack = d3.pack()
-      .size([width, height])
-      .padding(5)
-      (d3.hierarchy({ children: data })
-        .sum(d => d.count))
-
-    console.log(pack)
-
-    const svg = d3.select('.pie-chart')
-      .append('svg')
-      .attr('width', width)
-      .attr('height', height)
-      .call(responsivefy)
-
-    const packNode = svg.selectAll('.pack-node')
-      .data(pack.leaves())
-      .enter()
-      .append('g')
-      .attr('class', 'pack-node')
-      .attr('transform', d => `translate(${d.x}, ${d.y})`)
-
-    packNode
-      .append('circle')
-      .transition()
-      .duration(700)
-      .attr('r', d => d.r)
-      .style('fill', d => color(d.value / max))
-
-    packNode
-      .append('text')
-      .attr('font-size', 14)
-      .attr('class', 'fade-in')
-      .attr('text-anchor', 'middle')
-      .attr("dominant-baseline", "central")
-      .selectAll('tspan')
-      .data(d => d.data.key.split(' '))
-      .enter()
-      .append('tspan')
-        .attr('x', 0)
-        .attr('y', (d, i) => i * -14)
-        .attr('dy', (d, i, nodes) => nodes.length > 1 ? 14 : 0)
-        .text(t => t)
-  
-
-
-
-
-      //.call(responsivefy)
-
-    console.log(data)
+  selectLineView(ev) {
   }
 
   render() {
+    const { packSelect, lineSelect } = this.state
+    const { selectPackView, selectLineView } = this
+
+    console.log(this.props.thoughts)
+
     return (
       <div className='charts'>
         <h3>Analysis</h3>
-        
+
+        <h4>Your topics by popularity</h4>
+        <span className='select'>
+          <select name='packSelect' value={ packSelect } onChange={ selectPackView }>
+            <option value='recent'>Recent</option>
+            <option value='all'>All</option>
+          </select>
+        </span>
         <div className='chart pie-chart'>
         </div>
-        
+
+        <h4>Topics over time</h4>
+        <span className='select'>
+          <select name='packSelect' value={ lineSelect } onChange={ selectLineView }>
+            <option value=''>Recent</option>
+            <option value='all'>All</option>
+          </select>
+        </span>
+        <div className='chart line-chart'>
+        </div>
       </div>
     )
   }
---
+++ client/components/charts/Pack.js
@@ -0,0 +1,129 @@
+import * as d3 from 'd3'
+import { responsivefy } from '../../svghelpers'
+
+export default function Pack (data, container) {
+  // clear...
+  d3.select(container).select('svg').remove()
+
+  const max = d3.max(data, d => d.count),
+    color = d3.scaleLinear()
+      .domain([0, 1])
+      .range(['#80deea', '#f44336'])
+
+  var colors = d3.scaleOrdinal(d3.schemeCategory10)
+    .domain(d3.range(data.length));
+
+  const margin = { top: 20, right: 20, bottom: 20, left: 20 },
+    fullWidth = 300,
+    fullHeight = 300,
+    width = fullWidth - margin.right - margin.left,
+    height = fullHeight - margin.top - margin.bottom
+
+  const pack = d3.pack()
+    .size([width, height])
+    .padding(1.5)
+    (d3.hierarchy({ children: data })
+      .sum(d => d.count))
+
+  console.log(pack)
+
+  const svg = d3.select(container)
+    .append('svg')
+    .attr('width', fullWidth)
+    .attr('height', fullHeight)
+    .call(responsivefy)
+      .append('g')
+      .attr('transform', `translate(${margin.left}, ${margin.top})`)
+
+  const packNode = svg.selectAll('.pack-node')
+    .data(pack.leaves())
+    .enter()
+    .append('g')
+    .attr('class', 'pack-node')
+    .attr('transform', d => `translate(${d.x}, ${d.y})`)
+    .on('click', function (d, i, nodes) {
+      if (d3.selectAll('.pack-node.active').nodes().length) {
+        const active = d3.select('.pack-node.active')
+          .classed('active', false)
+
+        active
+          .select('text')
+          .transition()
+          .duration(500)
+          .style('opacity', 1)
+
+        active
+          .select('text.active')
+          .remove()
+
+        return d3.selectAll(nodes)
+          .transition()
+          .duration(300)
+          .style('opacity', 1)
+          .attr('transform', d => `translate(${d.x}, ${d.y})`)
+          .select('circle')
+            .transition()
+            .duration(500)
+            .attr('r', d => d.r)
+      }
+
+      d3.select(this)
+        .select('text')
+        .style('opacity', 0)
+
+      d3.selectAll(nodes)
+        .filter(n => n != d)
+        .transition()
+        .duration(300)
+        .style('opacity', 0)
+        .select('circle')
+          .transition()
+          .duration(300)
+          .attr('r', 0)
+
+      d3.select(this)
+        .classed('active', true)
+        .transition()
+        .duration(300)
+        .attr('transform', d => `translate(${width / 2}, ${height / 2})`)
+        .select('circle')
+          .transition()
+          .duration(500)
+          .attr('r', d => width / 2)
+      
+      setTimeout(() => {
+        d3.select(this)
+          .append('text')
+          .classed('active', true)
+          .selectAll('tspan')
+          .data(d => [d.data.key, d.value])
+          .enter()
+          .append('tspan')
+            .attr('x', 0)
+            .attr('y', (d, i) => i * 25)
+            .attr('dy', (d, i, nodes) => nodes.length > 1 ? -7 * (nodes.length - 1) : 0)
+            .text(t => t)
+      }, 750)
+
+    })
+
+  packNode
+    .append('circle')
+    .transition()
+    .duration(700)
+    .attr('r', d => d.r)
+    .style('fill', (d, i, nodes) => colors(d.value / nodes.length))
+
+  packNode
+    .append('text')
+    .attr('class', 'fade-in')
+    .attr("dominant-baseline", "central")
+    .selectAll('tspan')
+    .data(d => d.data.key.split(' '))
+    .enter()
+    .append('tspan')
+      .attr('x', 0)
+      .attr('y', (d, i) => i * 14)
+      .attr('dy', (d, i, nodes) => nodes.length > 1 ? -7 * (nodes.length - 1) : 0)
+      .text(t => t)
+}
