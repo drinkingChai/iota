b0d8ccd44e1b77b6f33f8bc3eed9cd7ac4bd81af
Wasif Zaman <teatocode@gmail.com>
2017-11-08 00:23:44 -0500

    wrappers

--- server/db/ThoughtWrapper.js
+++ server/db/ThoughtWrapper.js
@@ -89,6 +89,32 @@ Cluster.prototype.moveToHead = function(thought) {
     wrapper.nextNode -> this.headpreviousNode.
     head -> wrapper
   */
+  return Promise.all([
+    ThoughtWrapper.findOne({ where: { id: this.head } }),
+    ThoughtWrapper.findOne({ where: { clusterId: this.id, thoughtId: thought.id } }),
+  ])
+  .then(([ currentHead, thoughtWrapper ]) => (
+    Promise.all([
+      currentHead,
+      thoughtWrapper,
+      ThoughtWrapper.findOne({ where: { clusterId: this.id, thoughtId: thoughtWrapper.previousNode } }),
+      ThoughtWrapper.findOne({ where: { clusterId: this.id, thoughtId: thoughtWrapper.nextNode } })
+    ])
+  ))
+  .then(([ currentHead, thoughtWrapper, thoughtPrevious, thoughtNext ]) => {
+    // console.log(
+    //   currentHead,
+    //   thoughtWrapper.id,
+    //   thoughtPrevious && thoughtPrevious.id,
+    //   thoughtNext && thoughtNext.id)
+
+    return Promise.all([
+      thoughtPrevious && thoughtPrevious.update({ nextNode: thoughtWrapper.nextNode }),
+      thoughtNext && thoughtNext.update({ previousNode: thoughtWrapper.previousNode }),
+      thoughtWrapper.update({ previousNode: null, nextNode: this.head }),
+      currentHead.update({ previousNode: thoughtWrapper.id })
+    ])
+  })
 }
 
 Cluster.prototype.appendThought = function(thought) {
@@ -105,6 +131,35 @@ Cluster.prototype.appendThought = function(thought) {
 
 Cluster.prototype.removeThought = function(thought) {
   
+}
+
+
+
+
+/* ================================================= */
+/* ================= api wrappers  ================= */
+
+Cluster.makeCluster = function(clusterInfo, thoughtsIds) {
+  return Promise.all(thoughtsIds.map(id => Thought.findById(id)))
+    .then(thoughts => (
+      this.createCluster(clusterInfo, thoughts)
+    ))
+}
+
+Cluster.appendTo = function(clusterId, thoughtId) {
+  return this.findById(clusterId)
+    .then(cluster => (
+      Thought.findById(thoughtId)
+        .then(thought => cluster.appendThought(thought))
+    ))
+}
+
+Cluster.makeHead = function(clusterId, thoughtId) {
+  return this.findById(clusterId)
+    .then(cluster => (
+      Thought.findById(thoughtId)
+        .then(thought => cluster.moveToHead(thought))
+    ))
 }
 
 module.exports = { Cluster, Thought, ThoughtWrapper }
--- server/db/seedtest.js
+++ server/db/seedtest.js
@@ -12,18 +12,24 @@ const thought4 = { text: 'Thought 4' }
 const clusterInfo = { name: 'Thoughts 1,2,3' }
 
 const seed = () => {
+  let _cluster, _thought4
   return Promise.all(thoughts.map(t => Thought.create(t)))
-    .then(thoughts => Cluster.createCluster(clusterInfo, thoughts))
+    .then(thoughts => Cluster.makeCluster(clusterInfo, thoughts.map(t => t.id)))
     .then(() => Promise.all([
       Cluster.findOne({ where: { name: 'Thoughts 1,2,3' } }),
       Thought.findOne({ where: { text: 'Thought 1' } }),
       Thought.findOne({ where: { text: 'Thought 3' } })
     ]))
     .then(([ cluster, thought1, thought2 ]) => {
+      _cluster = cluster
       return cluster.moveAfter(thought1, thought2)
     })
-    // .then(() => Thought.create(thought4))
-    // .then(t => Cluster.appendThought(1, t))
+    .then(() => Thought.create(thought4))
+    .then(thought => {
+      _thought4 = thought
+      return Cluster.appendTo(_cluster.id, thought.id)
+    })
+    .then(() => Cluster.makeHead(_cluster.id, _thought4.id))
 }
 
 // seed prompt
