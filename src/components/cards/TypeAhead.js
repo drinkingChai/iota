74ffffdc37baff23035bae9c6714a544572f1db1
Wasif Zaman <teatocode@gmail.com>
2017-11-11 20:25:15 -0500

    phew

--- src/components/cards/TypeAhead.js
+++ src/components/cards/TypeAhead.js
@@ -2,7 +2,10 @@ import React, { Component } from 'react'
 
 export const Label = ({ text, onX }) => {
   return (
-    <span><span onClick={ onX }>X</span>{ text }</span>
+    <div className='label'>
+      <span className='label-button' onClick={ onX }><i className="im im-x-mark"></i></span>
+      <span className='label-text'>{ text }</span>
+    </div>
   )
 }
 
@@ -21,11 +24,14 @@ export default class TypeAhead extends Component {
     const { input, selections, selected } = this.state
     let { value } = ev.target
     if (value[value.length - 1] == ',' && input.length) {
-      value = value.slice(0, -1)
+      let sliced = value.slice(0, -1)
+
+      if (selected.indexOf(sliced) !== -1) return this.setState({ input: value })
+
       this.setState({
         input: '',
-        selections: [ ...selections, value ],
-        selected: [ ...selected, value ]
+        selections: [ ...selections, sliced ],
+        selected: [ ...selected, sliced ]
       })
     } else {
       this.setState({ input: value })
@@ -33,10 +39,11 @@ export default class TypeAhead extends Component {
   }
 
   onSelect = name => () => {
+    const { selected, selections } = this.state
     this.setState({
       input: '',
-      // selectionsDisplayed: false,
-      selected: [ ...this.state.selected, name ]
+      selectionsDisplayed: selections.length == selected.length + 1 ? false : true,
+      selected: [ ...selected, name ]
     })
   }
 
@@ -66,20 +73,24 @@ export default class TypeAhead extends Component {
       .filter(select => select.match(new RegExp(input, 'gi')))
 
     return (
-      <div>
-        <div>
-          <span>
-          {/* added items go here */}
-          { selected.map(select => <Label key={ select } text={ select } onX={ onDeselect(select) } />) }
+      <div className='typeahead'>
+        <div className='main'>
+          <span className='selected'>
+            {/* added items go here */}
+            { selected.map(select => <Label key={ select } text={ select } onX={ onDeselect(select) } />) }
+            <input value={ input } onChange={ onChange() } />
           </span>
 
-          <input value={ input } onChange={ onChange() }/>
-          <span onClick={ clear }>X</span>
-          <span onClick={ toggleDropdown }>{ selectionsDisplayed ? 'Up' : 'Down' }</span>
+          <span className='buttons'>
+            <span onClick={ clear }><i className="im im-x-mark"></i></span>
+            <span onClick={ toggleDropdown }>
+              { selectionsDisplayed ? <i className="im im-care-up"></i> : <i className="im im-care-down"></i> }
+            </span>
+          </span>
         </div>
 
         { input.length || selectionsDisplayed ?
-          <div>
+          <div className='selections'>
           { /* selections here */}
           { selections.map(select => <div key={ select } onClick={ onSelect(select) }>{ select }</div>) }
           </div> : null
